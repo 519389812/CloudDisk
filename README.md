9dc31ee1be2aa41427eac9b053cf888a035111c1
519389812 <519389812@qq.com>
2022-05-15 22:03:49 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # CloudDisk
 > 轻量级云盘系统
 
+使用到的库
+```text
+go get xorm.io/xorm
+
+go install github.com/zeromicro/go-zero/tools/goctl@latest
+goctl migrate —verbose —version v1.3.3
+```
+
 使用到的命令
-> 
+```text
+goctl api new core
+
+```
--- test/test.go
+++ test/test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"CloudDisk/models"
+	"bytes"
+	"encoding/json"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
 	"xorm.io/xorm"
@@ -10,15 +12,41 @@ import (
 var engine *xorm.Engine
 
 func main() {
+	fmt.Println("Start.")
 	var err error
-	engine, err = xorm.NewEngine("sqlite3", "/Cloud-disk.db")
+	engine, err = xorm.NewEngine("sqlite3", "Cloud-disk.db")
 	if err != nil {
-		return
+		fmt.Println("engine error")
+		fmt.Println(err)
 	}
 	data := make([]*models.UserBasic, 0)
+
+	//tbs, err := engine.DBMetas()
+	//if err != nil {
+	//	fmt.Println("db error")
+	//	fmt.Println(err)
+	//	return
+	//}
+	//for i, tb := range tbs {
+	//	fmt.Println("index:", i, "tbName", tb.Name)
+	//}
 	err = engine.Find(&data)
 	if err != nil {
-		return
+		fmt.Println("find error")
+		fmt.Println(err)
 	}
 	fmt.Println(data)
+
+	b, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("marshal error")
+		fmt.Println(err)
+	}
+	dst := new(bytes.Buffer)
+	err = json.Indent(dst, b, "", " ")
+	if err != nil {
+		fmt.Println("indent error")
+		fmt.Println(err)
+	}
+	fmt.Println(dst.String())
 }
